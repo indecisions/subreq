103f8ce4339c69e0142e0a7661d75f97d61aeb60
indecisions <43000704+indecisions@users.noreply.github.com>
2018-10-26 13:16:15 -0300

    Update subreq.py

--- src/subreq.py
+++ src/subreq.py
@@ -1,29 +1,27 @@
-#jaden
-import Tkinter as tk
-from Tkinter import *
+import tkinter as tk
+from tkinter import *
 from bs4 import BeautifulSoup
 import requests
-import subprocess
 import time
 
 def Main():
 
-	subprocess.call('clear')
-	input1 = raw_input("Web Address: ")
+	input1 = input("Web Address: ")
 	
 	window = tk.Tk()
 	window.configure(background="black")
-	window.title("nigger")
+	window.title("Subreq
+                     ")
 	window.geometry("620x520")
 
 	title = tk.Label(bg="black", fg="white", text="skrt skrt")
 	title.place(x=210, y=60)
 
-	Links = "--#Links#--"
-	Paragrahps = "--#Paragrahps#--"
-	Comments = "--#Comments#--"
-	hypertext1 = "--#Text[<h1>#--"
-	tables = "--#Tables#--"
+	Links = "@Links"
+	Paragrahps = "@Paragrahps"
+	Comments = "@Comments"
+	tables = "@Tables"
+	images = "@Images"
 
 	try:
 		r = requests.get(input1)
@@ -38,10 +36,10 @@ def Main():
 
 	for link1 in soup.find_all("a"):
 		for link2 in soup.find_all("p"):
-			for link3 in soup.find_all("br"):
+			for link3 in soup.find_all("img"):
 				for link4 in soup.find_all('table'):
 
-					results = Links, "\n\n", link1, "\n\n", Paragrahps, "\n\n", link2, "\n\n", Comments, "\n\n", hypertext1, "\n\n", tables, "\n\n", link4
+					results = Links, "\n\n", link1, "\n\n\n", Paragrahps, "\n\n\n", link2, "\n\n\n", images, "\n\n\n", link3, "\n\n\n", tables, "\n\n\n", link4, "\n\n\n",  
 			text = tk.Text(height=25, width=70)
 			text.place(x=30, y=50)
 			text.insert(tk.END, results,)
@@ -50,3 +48,6 @@ def Main():
 		window.mainloop()
 
 Main()
+
+#made by @ja8den
+
